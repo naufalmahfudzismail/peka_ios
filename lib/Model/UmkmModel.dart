45f7c77c460b2ef407a813c20b8ab7bf6d775db6
Naufal Mahfudz Ismail <naufalmahfudzismail@gmail.com>
2019-04-29 20:49:40 +0700

    UI Login

---
+++ lib/Model/UmkmModel.dart
@@ -0,0 +1,8 @@
+class UmkmModel {
+  final String namaUsaha;
+  final String kategori;
+  final String namaPemilik;
+  final String deskripsi;
+
+  UmkmModel(this.namaUsaha, this.kategori, this.namaPemilik, this.deskripsi);
+}
---
+++ lib/Model/UserModel.dart
@@ -0,0 +1,8 @@
+class UserModel {
+  
+  final String email;
+  final String nama;
+  final String status;
+
+  UserModel(this.email, this.nama, this.status);
+}
